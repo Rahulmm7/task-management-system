f1338b4e28a18c6c43e2e3b63fd3a87e15c17361
Rahul M M <90396638+Rahulmm7@users.noreply.github.com>
2022-01-24 08:35:37 +0530

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,8 +1,9 @@
 const { totp } = require('otplib');
-
+require('dotenv').config()
+const secretString = process.env.OTP_STRING
 
 function main() {
-    const secret = 'KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD'
+    const secret = secretString
     const otp = totp.generate(secret);
     return otp
 }
