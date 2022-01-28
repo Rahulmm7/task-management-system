0161bcfc47e156ed79dd96bdd962fce579fa67de
Rahul M M <90396638+Rahulmm7@users.noreply.github.com>
2022-01-28 22:18:09 +0530

    new packages and cleaned code
    added cors and helmet packages.
    required dotenv.

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const adminRouter = require('./routes/admin');
+const usersRouter = require('./routes/users');
+require('dotenv').config();
 
-var adminRouter = require('./routes/admin');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-//Database Connection
+// Database Connection
 const db = require('./config/database.js');
+
 db.authenticate().then(() => {
   console.log('Database connected...');
 }).catch(err => {
   console.log('First Error: ' + err);
 })
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors());
 
 app.use('/admin', adminRouter);
 app.use('/users', usersRouter);
