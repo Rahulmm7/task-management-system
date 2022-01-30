fc0871376a79d3fdf51b262bc9cc77bf43e113f8
Rahul M M <90396638+Rahulmm7@users.noreply.github.com>
2022-01-30 22:50:17 +0530

    Added reminder route
    Added reminder route and standardized the code

--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 
-const { create, login, user_emailVerify, user_mobileVerify, task, subtask, taskUpdate, subtaskUpdate, taskDelete, subtaskDelete, getAllTasks, logout } = require('../controller/user_controller')
-const { validateCreateTask, validateCreatesubTask, validateTaskUpdate, validateUserCreate, validateUserLogin, validatesubtaskUpdate } = require('../middleware/validator');
+const controller = require('../controller/user_controller')
+const validate = require('../middleware/validator');
 const auth = require('../middleware/authenication');
 
-router.post('/signup', validateUserCreate, create);
-router.post('/login', validateUserLogin, login);
-router.post('/emailVerify', user_emailVerify);
-router.post('/mobileVerify', user_mobileVerify);
-router.post('/task', auth, validateCreateTask, task);
-router.post('/subtask', auth, validateCreatesubTask, subtask);
-router.put('/taskUpdate', auth, validateTaskUpdate, taskUpdate);
-router.put('/subtaskUpdate', auth, validatesubtaskUpdate, subtaskUpdate);
-router.delete('/taskdelete/:id', auth, taskDelete);
-router.delete('/subtaskdelete/:id&:taskID', auth, subtaskDelete);
-router.get('/getAll', auth, getAllTasks);
-router.get('/logout', logout);
+router.post('/signup', validate.validateUserCreate, controller.create);
+router.post('/login', validate.validateUserLogin, controller.login);
+router.post('/emailVerify', controller.user_emailVerify);
+router.post('/mobileVerify', controller.user_mobileVerify);
+router.post('/task', auth, validate.validateCreateTask, controller.task);
+router.post('/subtask', auth, validate.validateCreatesubTask, controller.subtask);
+router.put('/taskUpdate', auth, validate.validateTaskUpdate, controller.taskUpdate);
+router.put('/subtaskUpdate', auth, validate.validatesubtaskUpdate, controller.subtaskUpdate);
+router.delete('/taskdelete/:id', auth, controller.taskDelete);
+router.delete('/subtaskdelete/:id&:taskID', auth, controller.subtaskDelete);
+router.get('/getAll', auth, controller.getAllTasks);
+router.post('/reminder', controller.reminder);
+router.get('/logout', controller.logout);
 
 module.exports = router;
