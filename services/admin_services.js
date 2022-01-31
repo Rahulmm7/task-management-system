8f82fb975f0b4f6917dfaef7c52c1014a5399c76
Rahul M M <90396638+Rahulmm7@users.noreply.github.com>
2022-01-31 12:07:09 +0530

    cleaned as per eslint

--- services/admin_services.js
+++ services/admin_services.js
@@ -8,86 +8,74 @@ const Task = require('../models/taskTable');
 
 const jwthashstring = process.env.JWTSTRING;
 
-
-//db querry to admin login
+// db querry to admin login
 
 exports.admin_login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        let user = await User.findOne({ where: { email: email } });
+        const user = await User.findOne({ where: { email } });
 
-        if (!user)
-            return responseFile.errorResponse(res, "no user found...", 404);
+        if (!user) { return responseFile.errorResponse(res, 'no user found...', 404); }
 
         if (!user.isAdmin) {
-            return responseFile.errorResponse(res, "You are not admin !", 403);
+            return responseFile.errorResponse(res, 'You are not admin !', 403);
         }
 
-        if (!user.isEmailVerified)
-            return responseFile.errorResponse(res, "email not verified...", 403)
+        if (!user.isEmailVerified) { return responseFile.errorResponse(res, 'email not verified...', 403); }
 
-        if (!user.isMobileVerified)
-            return responseFile.errorResponse(res, "Mobile not verified...", 403);
+        if (!user.isMobileVerified) { return responseFile.errorResponse(res, 'Mobile not verified...', 403); }
 
-        if (user.userStatus === "Blocked") {
-            return responseFile.errorResponse(res, "Your account is blocked", 403);
+        if (user.userStatus === 'Blocked') {
+            return responseFile.errorResponse(res, 'Your account is blocked', 403);
         }
-        if (user.userStatus === "Deleted") {
-            return responseFile.errorResponse(res, "User unavailable. Your account may be deleted", 403);
+        if (user.userStatus === 'Deleted') {
+            return responseFile.errorResponse(res, 'User unavailable. Your account may be deleted', 403);
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch)
-            return responseFile.errorResponse(res, "Invalid password...", 403)
+        if (!isMatch) { return responseFile.errorResponse(res, 'Invalid password...', 403); }
 
-        //creating payload 
+        // creating payload
         const payload = {
-            user: { id: user.userID }
+            user: { id: user.userID },
         };
 
-        //creating token using payload
-        let token = await jwt.sign(payload, jwthashstring, { expiresIn: 3600 }, { algorithm: 'RS256' })
+        // creating token using payload
+        const token = await jwt.sign(payload, jwthashstring, { expiresIn: 3600 }, { algorithm: 'RS256' });
 
-        //storing email in redis using token as keyword
-        let redisData = await client.set(token, email, 'EX', 3600);
+        // storing email in redis using token as keyword
+        const redisData = await client.set(token, email, 'EX', 3600);
 
-        return token
+        return token;
     } catch (error) {
-        console.log("error", err)
-        return responseFile.errorResponse(res, "Something went wrong...", 400)
-
+        console.log('error', error);
+        return responseFile.errorResponse(res, 'Something went wrong...', 400);
     }
-}
+};
 
+// db querry of admin page to view all tasks
 
-//db querry of admin page to view all tasks
-
-exports.adminGetAllTasks = async (param) => {
-
+exports.adminGetAllTasks = async (param, res) => {
     try {
-        let tasklist = await Task.findAll({});
+        const tasklist = await Task.findAll({});
         return tasklist;
-
     } catch (error) {
-        responseFile.errorResponse(res, "Server issue", 500)
-        return
+        responseFile.errorResponse(res, 'Server issue', 500);
     }
-}
+};
 
-//db querry to admin logout
+// db querry to admin logout
 
 exports.admin_logout = async (req, res) => {
-    const loginToken = req.header("token");
+    const loginToken = req.header('token');
     try {
-        client.del(loginToken, function (err, response) {
-            if (response == 1) {
-                return responseFile.successResponse(res, "user logout...");
-            } else {
-                return responseFile.errorResponse(res, "something went wrong !!!", 400)
+        client.del(loginToken, (err, response) => {
+            if (response === 1) {
+                return responseFile.successResponse(res, 'user logout...');
             }
-        })
-
+            return responseFile.errorResponse(res, 'something went wrong !!!', 400);
+        });
     } catch (error) {
-        return responseFile.errorResponse(res, "server Error!!!", 500);
+        return responseFile.errorResponse(res, 'server Error!!!', 500);
     }
-}
+};
