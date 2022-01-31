6ca034d731d8c62761b04bb0e7b331df6542be95
Rahul M M <90396638+Rahulmm7@users.noreply.github.com>
2022-01-31 11:59:37 +0530

    cleaned as per eslint

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -1,45 +1,39 @@
 const admin_services = require('../services/admin_services');
 const responseFile = require('../response');
 
-
 exports.login = async (req, res) => {
     try {
-        let response = await admin_services.admin_login(req, res);
+        const response = await admin_services.admin_login(req, res);
         if (response) {
-            responseFile.successResponse(res, response)
-            return
-        }
-        else {
-            console.log("invalid/no response from Function admin_login!!!")
+            responseFile.successResponse(res, response);
+        } else {
+            console.log('invalid/no response from Function admin_login!!!');
         }
     } catch (error) {
-        console.log("error", error);
-        return responseFile.errorResponse(res, "invalid credentinals !!!", 400)
+        console.log('error', error);
+        return responseFile.errorResponse(res, 'invalid credentinals !!!', 400);
     }
-}
+};
 
 exports.getAllTasks = async (req, res) => {
     try {
-        let response = await admin_services.adminGetAllTasks(req, res);
-        if (response.length == 0) {
-            return responseFile.errorResponse(res, "no users in the list", 404)
+        const response = await admin_services.adminGetAllTasks(req, res);
+        if (response.length === 0) {
+            responseFile.errorResponse(res, 'no users in the list', 404);
+        } else {
+            responseFile.successResponse(res, response, 'List of users');
         }
-        else {
-            return responseFile.successResponse(res, response, "List of users");
-        }
-
     } catch (error) {
-        return responseFile.errorResponse(res, "currently no data available !!!", 400)
+        return responseFile.errorResponse(res, 'currently no data available !!!', 400);
     }
-}
+};
 
-//admin logout
+// admin logout
 
 exports.logout = async (req, res) => {
     try {
-        let response = await admin_services.admin_logout(req, res);
-
+        const response = await admin_services.admin_logout(req, res);
     } catch (error) {
-        return responseFile.errorResponse(res, "Something went wrong!!!", 400);
+        return responseFile.errorResponse(res, 'Something went wrong!!!', 400);
     }
-}
+};
