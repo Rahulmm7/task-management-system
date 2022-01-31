2e2a9c99c7e7cf906a0fa73795e12ded9bb36556
Rahul M M <90396638+Rahulmm7@users.noreply.github.com>
2022-01-31 12:12:07 +0530

    cleaned as per eslint

--- utils/twillio.js
+++ utils/twillio.js
@@ -2,15 +2,15 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-let sms = async (mobile, message) => {
+const sms = async (mobile, message) => {
     client.messages
         .create({
             body: message,
             from: '+14696207863',
-            to: mobile
+            to: mobile,
         })
-        .then(message => console.log(message.sid))
+        .then((message) => console.log(message.sid))
         .catch((err) => console.log(err));
-}
+};
 
 module.exports = sms;
